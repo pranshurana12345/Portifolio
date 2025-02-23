aef3716ec5943003a5033d8baed3f4d691dddae7
Pritam <debnathpritam0802@gmail.com>
2025-02-23 17:46:46 +0530

    added cursor

---
+++ scripts/cursor.script.js
@@ -0,0 +1,17 @@
+const cursor = document.querySelector('.cursor');
+
+
+function moveCursor(x, y) {
+  cursor.style.transform = `translate(${x - 25}px, ${y - 25}px)`;
+}
+
+
+document.addEventListener('mousemove', (e) => {
+  moveCursor(e.clientX, e.clientY);
+});
+
+
+document.addEventListener('touchmove', (e) => {
+  const touch = e.touches[0]; // Get first touch point
+  moveCursor(touch.clientX, touch.clientY);
+});

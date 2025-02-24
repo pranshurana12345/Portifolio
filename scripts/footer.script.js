37071b0f035d7789edab5dfdb527c55b6efa2ad2
Pritam <debnathpritam0802@gmail.com>
2025-02-24 19:12:56 +0530

    changes

--- scripts/footer.script.js
+++ scripts/footer.script.js
@@ -1,14 +1,15 @@
 const duration = getComputedStyle(document.documentElement).getPropertyValue('--fall-duration');
 
 function createFallingStars() {
-  let footer = document.querySelector('footer');
+  let footer = document.querySelector('.footer-container');
   let i = document.createElement('i');
   i.style.height = `${Math.random() * 5 + 2}rem`;
   i.style.width = `0.2rem`;
   i.style.left = `${Math.random() * 150}vw`;
   i.style.background = `linear-gradient(transparent, snow)`;
-  i.style.animationDuration = `${Math.random() * parseFloat(duration) + 2}s`;
+  i.style.animationDuration = `${Math.random() * parseFloat(duration) + 6}s`;
   i.style.animationDelay = `${Math.random() *  10 }s`;
+  i.style.opacity = `${Math.random() }`;
   footer.appendChild(i);
   /*
   i.addEventListener('animationend', () => {

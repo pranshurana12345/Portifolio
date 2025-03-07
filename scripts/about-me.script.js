c63fb38cca337d5f5930e038006da80b302d81e1
Pritam <debnathpritam0802@gmail.com>
2025-03-07 12:17:11 +0530

    added about me section

---
+++ scripts/about-me.script.js
@@ -0,0 +1,25 @@
+//
+let aboutMe = `
+I'm a Front-End Developer with over 2 years of fulltime work experience,
+ specializing in web application development. 
+I'm passionate about crafting engaging user experiences 
+and leveraging cutting-edge technologies to bring ideas to life`
+let aboutMeSection = document.getElementById('about-me-section')
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            fadeTypingAnimation(aboutMeSection,
+                aboutMe
+                , 100);
+            observer.unobserve(entry.target); // Stop observing once animated
+        }
+    });
+}, { threshold: 0.2, rootMargin: "50px 0px" }); // Slight margin for smoother triggering
+
+
+
+
+
+observer.observe(aboutMeSection);
